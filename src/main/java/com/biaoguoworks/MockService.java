7fda02e863ccb18bbdb29a98f3380de6ae23197f
wuxin <myeagleandsnake@gaml.com>
2025-07-17 01:47:06 +0800

    feat

--- src/main/java/com/biaoguoworks/MockService.java
+++ src/main/java/com/biaoguoworks/MockService.java
@@ -1,13 +1,7 @@
 package com.biaoguoworks;
 
 import jakarta.annotation.Resource;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -19,13 +13,21 @@ public class MockService {
 
     @Resource
     private UserMapper userMapper;
+    @Resource
+    private MockService mockService;
 
     @Transactional(rollbackFor = Exception.class)
     public void insertSomeUsers() throws InterruptedException {
         userMapper.insertUser("我是主");
-        Thread thread = Thread.startVirtualThread(() -> userMapper.insertUser("我是虚拟"));
+        Thread thread = Thread.startVirtualThread(() ->mockService.Insert2() );
         thread.join();
         System.out.println(1 / 0);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void Insert2(){
+        userMapper.insertUser("我是虚拟");
+
     }
 
 
