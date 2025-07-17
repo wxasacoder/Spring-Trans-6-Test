ae93792fb40d0ded123065a1ec59fe07dc406481
WUXIN <myeagleandsnake@gmail.com>
2025-07-17 10:18:39 +0800

    feat

--- src/main/java/com/biaoguoworks/MockService.java
+++ src/main/java/com/biaoguoworks/MockService.java
@@ -2,7 +2,9 @@ package com.biaoguoworks;
 
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 
 /**
  * @author wuxin
@@ -15,16 +17,24 @@ public class MockService {
     private UserMapper userMapper;
     @Resource
     private MockService mockService;
+    @Resource
+    private PlatformTransactionManager platformTransactionManager;
+    @Resource
+    private TransactionDefinition transactionDefinition;
 
-    @Transactional(rollbackFor = Exception.class)
-    public void insertSomeUsers() throws InterruptedException {
-        userMapper.insertUser("我是主");
-        Thread thread = Thread.startVirtualThread(() ->mockService.Insert2() );
-        thread.join();
-        System.out.println(1 / 0);
+    public void insertSomeUsers() {
+        TransactionStatus transaction = platformTransactionManager.getTransaction(transactionDefinition);
+        try {
+            userMapper.insertUser("我是主");
+            Thread thread = Thread.startVirtualThread(() ->mockService.Insert2() );
+            thread.join();
+            System.out.println(1 / 0);
+            platformTransactionManager.commit(transaction);
+        }catch (Exception e){
+            platformTransactionManager.rollback(transaction);
+        }
     }
 
-    @Transactional(rollbackFor = Exception.class)
     public void Insert2(){
         userMapper.insertUser("我是虚拟");
 
